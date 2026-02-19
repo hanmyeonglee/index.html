58c5f0e8124082cfe63464b65124505c883e0c49
Nemo <ejongwon7@gmail.com>
2026-02-19 23:25:54 +0900

    Refactor: delete useless variables and integrate similar one

--- src/App.ts
+++ src/App.ts
@@ -5,6 +5,8 @@ import gsap from 'gsap'
 import "./App.css";
 import './ascii-cube.js'
 
+type IntroPhase = 'draw' | 'pulse' | 'stable' | 'typing';
+
 const INTRO_CONFIG = {
     geometry: {
         ringRadius: 45,
@@ -75,16 +77,10 @@ export class App extends LitElement {
     private sweepAngle = 0;
 
     @state()
-    private armOpacity = 1;
+    private pulseLevel = 0;
 
     @state()
-    private ringScale = 1;
-
-    @state()
-    private strokeScale = 1;
-
-    @state()
-    private glowScale = 1;
+    private phase: IntroPhase = 'draw';
 
     @state()
     private shakeOffsetX = 0;
@@ -96,23 +92,9 @@ export class App extends LitElement {
     private loaderAngle = INTRO_CONFIG.loader.initialAngleDeg;
 
     @state()
-    private isStablePhase = false;
-
-    @state()
-    private typingVisible = false;
-
-    @state()
     private typedMessage = '';
 
-    private drawTween?: gsap.core.Tween;
-    private pulseTimeline?: gsap.core.Timeline;
-    private shakeTween?: gsap.core.Tween;
-    private flashTimeline?: gsap.core.Timeline;
-    private stablePulseTween?: gsap.core.Tween;
-    private loaderTween?: gsap.core.Tween;
-    private cubeTween?: gsap.core.Tween;
-    private sideGlowTween?: gsap.core.Tween;
-    private typingTimeline?: gsap.core.Timeline;
+    private masterTimeline?: gsap.core.Timeline;
 
     @query('#intro-flash')
     private readonly flashLayer!: HTMLDivElement;
@@ -136,141 +118,133 @@ export class App extends LitElement {
 
     disconnectedCallback(): void {
         super.disconnectedCallback();
-        this.drawTween?.kill();
-        this.pulseTimeline?.kill();
-        this.shakeTween?.kill();
-        this.flashTimeline?.kill();
-        this.stablePulseTween?.kill();
-        this.loaderTween?.kill();
-        this.cubeTween?.kill();
-        this.sideGlowTween?.kill();
-        this.typingTimeline?.kill();
-        this.drawTween = undefined;
-        this.pulseTimeline = undefined;
-        this.shakeTween = undefined;
-        this.flashTimeline = undefined;
-        this.stablePulseTween = undefined;
-        this.loaderTween = undefined;
-        this.cubeTween = undefined;
-        this.sideGlowTween = undefined;
-        this.typingTimeline = undefined;
+        this.masterTimeline?.kill();
+        this.masterTimeline = undefined;
     }
 
     private startIntroRingAnimation(): void {
+        this.masterTimeline?.kill();
+
         this.sweepAngle = 0;
-        this.armOpacity = 1;
-        this.ringScale = 1;
-        this.strokeScale = 1;
-        this.glowScale = 1;
+        this.pulseLevel = 0;
+        this.phase = 'draw';
         this.shakeOffsetX = 0;
         this.shakeOffsetY = 0;
         this.loaderAngle = INTRO_CONFIG.loader.initialAngleDeg;
-        this.isStablePhase = false;
-        this.typingVisible = false;
         this.typedMessage = '';
         gsap.set(this.cubeLayer, {
             opacity: 0,
             '--intro-cube-scale': 0.94,
         });
         gsap.set([this.sideGlowLeft, this.sideGlowRight], { opacity: 0 });
+        gsap.set(this.flashLayer, { display: 'none', opacity: 0 });
 
         const target = { value: 0 };
-        this.drawTween?.kill();
-        this.drawTween = gsap.to(target, {
+        const master = gsap.timeline();
+
+        master.to(target, {
             value: 360,
             duration: INTRO_CONFIG.draw.durationMs / 1000,
             ease: INTRO_CONFIG.draw.ease,
             onUpdate: () => {
                 this.sweepAngle = target.value;
-                this.armOpacity = this.calculateArmOpacity(this.sweepAngle);
-            },
-            onComplete: () => {
-                this.sweepAngle = 360;
-                this.armOpacity = 0;
-                this.startPulsePhase();
             },
         });
+
+        master.call(() => {
+            this.sweepAngle = 360;
+            this.phase = 'pulse';
+        });
+
+        master.add(this.createPulseTimeline());
+
+        master.call(() => {
+            this.phase = 'stable';
+        });
+
+        master.add(this.createStableLoopTimeline(), '<');
+
+        master.to(this.cubeLayer, {
+            opacity: 1,
+            '--intro-cube-scale': 1,
+            duration: 0.9,
+            ease: 'power2.out',
+        }, '<');
+
+        master.call(() => {
+            this.phase = 'typing';
+            this.typedMessage = '';
+        }, [], `>+${INTRO_CONFIG.typing.startDelayAfterCubeMs / 1000}`);
+
+        const message = INTRO_CONFIG.typing.message;
+        for (let idx = 1; idx <= message.length; idx++) {
+            master.call(() => {
+                this.typedMessage = message.slice(0, idx);
+            });
+
+            if (idx < message.length) {
+                master.to({}, { duration: INTRO_CONFIG.typing.charDelayMs / 1000 });
+            }
+        }
+
+        this.masterTimeline = master;
     }
 
-    private startPulsePhase(): void {
-        this.pulseTimeline?.kill();
-
+    private createPulseTimeline(): gsap.core.Timeline {
         const timeline = gsap.timeline();
 
         for (let idx = 0; idx < INTRO_CONFIG.pulse.count; idx++) {
             const isLastPulse = idx === INTRO_CONFIG.pulse.count - 1;
             const intensity = (idx + 1) / INTRO_CONFIG.pulse.count;
-            const growScale = 1 + INTRO_CONFIG.pulse.ringScaleGain * intensity;
-            const growStroke = 1 + INTRO_CONFIG.pulse.strokeScaleGain * intensity;
-            const growGlow = 1 + INTRO_CONFIG.pulse.glowScaleGain * intensity;
             const shakeAmp = this.lerp(INTRO_CONFIG.pulse.shakeAmplitudeMin, INTRO_CONFIG.pulse.shakeAmplitudeMax, intensity);
             const growSec = INTRO_CONFIG.pulse.expandDurationMs / 1000;
             const settleSec = INTRO_CONFIG.pulse.settleDurationMs / 1000;
+            const shakeDurationSec = growSec + settleSec;
 
             timeline.to(this, {
-                ringScale: growScale,
-                strokeScale: growStroke,
-                glowScale: growGlow,
+                pulseLevel: intensity,
                 duration: growSec,
                 ease: 'power2.out',
-                onStart: () => {
-                    this.startShake(shakeAmp, growSec + settleSec);
-                },
             });
 
+            timeline.to({}, {
+                duration: shakeDurationSec,
+                ease: 'none',
+                onUpdate: () => {
+                    this.shakeOffsetX = gsap.utils.random(-shakeAmp, shakeAmp, 0.01);
+                    this.shakeOffsetY = gsap.utils.random(-shakeAmp, shakeAmp, 0.01);
+                },
+                onComplete: () => {
+                    this.shakeOffsetX = 0;
+                    this.shakeOffsetY = 0;
+                },
+            }, `<`);
+
             if (isLastPulse) {
-                timeline.call(() => {
-                    this.triggerFlash();
-                }, [], '>');
+                timeline.add(this.createFlashTimeline(), '>');
             }
 
             timeline.to(this, {
-                ringScale: 1,
-                strokeScale: 1,
-                glowScale: 1,
+                pulseLevel: 0,
                 duration: settleSec,
                 ease: 'power2.inOut',
             });
         }
 
-        this.pulseTimeline = timeline;
+        return timeline;
     }
 
-    private startShake(amplitude: number, durationSec: number): void {
-        this.shakeTween?.kill();
-
-        this.shakeTween = gsap.to({}, {
-            duration: durationSec,
-            ease: 'none',
-            onUpdate: () => {
-                this.shakeOffsetX = gsap.utils.random(-amplitude, amplitude, 0.01);
-                this.shakeOffsetY = gsap.utils.random(-amplitude, amplitude, 0.01);
+    private createFlashTimeline(): gsap.core.Timeline {
+        const timeline = gsap.timeline({
+            onStart: () => {
+                gsap.set(this.flashLayer, { display: 'block', opacity: 0 });
             },
-            onComplete: () => {
-                gsap.to(this, {
-                    shakeOffsetX: 0,
-                    shakeOffsetY: 0,
-                    duration: 0.08,
-                    ease: 'power2.out',
-                });
-            },
-        });
-    }
-
-    private triggerFlash(): void {
-        this.flashTimeline?.kill();
-        this.sideGlowTween?.kill();
-
-        this.flashTimeline = gsap.timeline({
             onComplete: () => {
                 gsap.set(this.flashLayer, { display: 'none' });
-                this.enterStablePhase();
             },
         });
 
-        gsap.set(this.flashLayer, { display: 'block', opacity: 0 });
-
-        this.flashTimeline
+        timeline
             .to(this.flashLayer, {
                 opacity: INTRO_CONFIG.flash.maxOpacity,
                 duration: INTRO_CONFIG.flash.fadeInMs / 1000,
@@ -291,79 +265,29 @@ export class App extends LitElement {
                 duration: 0.52,
                 ease: 'power2.out',
             }, '<');
+
+        return timeline;
     }
 
-    private enterStablePhase(): void {
-        this.isStablePhase = true;
-        this.startStablePulse();
-        this.startLoaderRotation();
-        this.fadeInCube();
-    }
+    private createStableLoopTimeline(): gsap.core.Timeline {
+        const stableLoop = gsap.timeline();
 
-    private startStablePulse(): void {
-        this.stablePulseTween?.kill();
-
-        this.stablePulseTween = gsap.to(this, {
-            ringScale: 1 + INTRO_CONFIG.stablePulse.ringScaleGain,
-            strokeScale: 1 + INTRO_CONFIG.stablePulse.strokeScaleGain,
-            glowScale: 1 + INTRO_CONFIG.stablePulse.glowScaleGain,
+        stableLoop.add(gsap.to(this, {
+            pulseLevel: 1,
             duration: INTRO_CONFIG.stablePulse.durationMs / 1000,
             ease: INTRO_CONFIG.stablePulse.ease,
             repeat: -1,
             yoyo: true,
-        });
-    }
+        }), 0);
 
-    private startLoaderRotation(): void {
-        this.loaderTween?.kill();
-
-        this.loaderTween = gsap.to(this, {
-            loaderAngle: this.loaderAngle + 360,
+        stableLoop.add(gsap.to(this, {
+            loaderAngle: '+=360',
             duration: INTRO_CONFIG.loader.cycleDurationMs / 1000,
             ease: INTRO_CONFIG.loader.ease,
             repeat: -1,
-            onRepeat: () => {
-                this.loaderAngle -= 360;
-            },
-        });
-    }
+        }), 0);
 
-    private fadeInCube(): void {
-        this.cubeTween?.kill();
-
-        this.cubeTween = gsap.to(this.cubeLayer, {
-            opacity: 1,
-            '--intro-cube-scale': 1,
-            duration: 0.9,
-            ease: 'power2.out',
-            onComplete: () => {
-                this.startTyping();
-            },
-        });
-    }
-
-    private startTyping(): void {
-        this.typingTimeline?.kill();
-        this.typingVisible = true;
-        this.typedMessage = '';
-
-        const message = INTRO_CONFIG.typing.message;
-        const timeline = gsap.timeline({
-            delay: INTRO_CONFIG.typing.startDelayAfterCubeMs / 1000,
-        });
-
-        let cursor = 0;
-        for (let idx = 1; idx <= message.length; idx++) {
-            timeline.call(() => {
-                this.typedMessage = message.slice(0, idx);
-            }, [], cursor);
-
-            if (idx < message.length) {
-                cursor += INTRO_CONFIG.typing.charDelayMs / 1000;
-            }
-        }
-
-        this.typingTimeline = timeline;
+        return stableLoop;
     }
 
     private lerp(start: number, end: number, t: number): number {
@@ -432,20 +356,26 @@ export class App extends LitElement {
         const ringPath = this.getRingPath();
         const center = 50;
         const armEnd = this.getRingEndPoint();
-        const shouldShowArm = this.armOpacity > 0.01;
-        const glowBlurScale = 1 + (this.glowScale - 1) * INTRO_CONFIG.glow.blurGain;
+        const armOpacity = this.phase === 'draw' ? this.calculateArmOpacity(this.sweepAngle) : 0;
+        const shouldShowArm = armOpacity > 0.01;
+        const isStableLike = this.phase === 'stable' || this.phase === 'typing';
+        const scaleConfig = isStableLike ? INTRO_CONFIG.stablePulse : INTRO_CONFIG.pulse;
+        const ringScale = 1 + scaleConfig.ringScaleGain * this.pulseLevel;
+        const strokeScale = 1 + scaleConfig.strokeScaleGain * this.pulseLevel;
+        const glowScale = 1 + scaleConfig.glowScaleGain * this.pulseLevel;
+        const glowBlurScale = 1 + (glowScale - 1) * INTRO_CONFIG.glow.blurGain;
         const mainGlowBlur = INTRO_CONFIG.glow.blurStdDeviation * glowBlurScale;
-        const mainGlowAlpha = Math.min(1, INTRO_CONFIG.glow.alpha + (this.glowScale - 1) * INTRO_CONFIG.glow.alphaGain);
-        const ringStrokeWidth = INTRO_CONFIG.geometry.ringStrokeWidth * this.strokeScale;
-        const armStrokeWidth = INTRO_CONFIG.arm.strokeWidth * this.strokeScale;
+        const mainGlowAlpha = Math.min(1, INTRO_CONFIG.glow.alpha + (glowScale - 1) * INTRO_CONFIG.glow.alphaGain);
+        const ringStrokeWidth = INTRO_CONFIG.geometry.ringStrokeWidth * strokeScale;
+        const armStrokeWidth = INTRO_CONFIG.arm.strokeWidth * strokeScale;
         const loaderPath = this.getLoaderArcPath();
-        const loaderGlowBlur = INTRO_CONFIG.loader.glow.blurStdDeviation * (0.9 + this.glowScale * 0.22);
-        const loaderGlowAlpha = Math.min(1, INTRO_CONFIG.loader.glow.alpha + (this.glowScale - 1) * 0.12);
+        const loaderGlowBlur = INTRO_CONFIG.loader.glow.blurStdDeviation * (0.9 + glowScale * 0.22);
+        const loaderGlowAlpha = Math.min(1, INTRO_CONFIG.loader.glow.alpha + (glowScale - 1) * 0.12);
         const sideGlowStyle = [
             `--intro-side-glow-blur: ${INTRO_CONFIG.sideGlow.blurPx}px`,
             `--intro-side-glow-tone: ${INTRO_CONFIG.sideGlow.tone}`,
         ].join('; ');
-        const scaleTransform = `translate(${center} ${center}) scale(${this.ringScale}) translate(${-center} ${-center})`;
+        const scaleTransform = `translate(${center} ${center}) scale(${ringScale}) translate(${-center} ${-center})`;
         const shakeTransform = `translate(${this.shakeOffsetX} ${this.shakeOffsetY})`;
 
         return html`
@@ -502,7 +432,7 @@ export class App extends LitElement {
                                             x2="${armEnd.x}"
                                             y2="${armEnd.y}"
                                             stroke-width="${armStrokeWidth}"
-                                            opacity="${this.armOpacity}"
+                                            opacity="${armOpacity}"
                                         ></line>
                                     `
                                     : null}
@@ -511,7 +441,7 @@ export class App extends LitElement {
                                     d="${ringPath}"
                                     stroke-width="${ringStrokeWidth}"
                                 />
-                                ${this.isStablePhase
+                                ${isStableLike
                                     ? svg`
                                         <path
                                             id="intro-ring-loader"
@@ -526,7 +456,7 @@ export class App extends LitElement {
                     <div id="intro-cube" class="absolute left-1/2 top-1/2 z-4 grid place-items-center opacity-0 pointer-events-none" aria-hidden="true">
                         <ascii-cube></ascii-cube>
                     </div>
-                    <div id="intro-typing" class="${this.typingVisible ? 'is-visible' : ''} absolute left-1/2 z-5 w-max -translate-x-1/2 opacity-0 pointer-events-none transition-opacity duration-300 ease-out" aria-live="polite">
+                    <div id="intro-typing" class="${this.phase === 'typing' ? 'is-visible' : ''} absolute left-1/2 z-5 w-max -translate-x-1/2 opacity-0 pointer-events-none transition-opacity duration-300 ease-out" aria-live="polite">
                         <div id="intro-typing-measure" class="invisible whitespace-pre font-mono text-2xl tracking-[0.02em]">$ ${INTRO_CONFIG.typing.message}_</div>
                         <div id="intro-typing-line" class="absolute inset-0 flex items-center whitespace-pre font-mono text-2xl tracking-[0.02em] text-[rgba(225,255,255,0.98)] [text-shadow:0_0_8px_rgba(116,237,255,0.58),0_0_18px_rgba(92,210,255,0.36)]">
                             <span id="intro-typing-prompt">$</span>
